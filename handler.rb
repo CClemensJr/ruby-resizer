d4915eb9141ebf9cce82669d3d3aaf01276b4d5d
Charles Clemens <cclemensjr@gmail.com>
2019-07-10 13:51:38 -0700

    Renamed class

--- handler.rb
+++ handler.rb
@@ -6,8 +6,8 @@ class Resizer
     bucket = event["s3"]["bucket"]["name"]
     object = event["s3"]["object"]["key"]
 
-    file = UploadedFile.from_s3(bucket, object)
+    file = UploadedImage.from_s3(bucket, object)
     file.resize "50x50"
-    file.uploaded_file("resized-images", "resized_" + event["s3"]["object"]["key"])
+    file.uploaded_image("resized-images", "resized_" + event["s3"]["object"]["key"])
   end
 end
