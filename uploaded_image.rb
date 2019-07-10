9f51516f59757a2ebd50ae56eba6874eb5bf9ce7
Charles Clemens <cclemensjr@gmail.com>
2019-07-10 14:25:21 -0700

    Added method comments

--- uploaded_image.rb
+++ uploaded_image.rb
@@ -2,6 +2,7 @@ require "aws-sdk-s3"
 require "mini_magick"
 
 class UploadedImage
+# This method takes a bucket identifier and an object identifier to get an image
   def self.s3_image(bucket_name, object_name)
     s3 = Aws::S3::Resource.new()
     object = s3.bucket(bucket_name).object(object_name)
@@ -12,14 +13,14 @@ class UploadedImage
     UploadedImage.new(tmp_image_name)
   end
 
-
+# This method creates an instance variable containing the image
   def initialize(tmp_image)
     @tmp_image = tmp_image
   end
 
-
+# This method takes the image using Minimagick and resizes it
   def resize_image(params)
-    image = MiniMagick::Image.open(@tmp_file)
+    image = MiniMagick::Image.open(@tmp_image)
 
     image.resize_image params
 
@@ -28,7 +29,7 @@ class UploadedImage
     image.write @resized_tmp_image
   end
 
-
+# This method takes the resized image and saves it into a different bucket for resized images
   def upload_image(target_bucket, target_object)
     s3 = Aws::S3::Resource.new()
 
