d3c904f4990416e633827be922aecc9c6169134e
Charles Clemens <cclemensjr@gmail.com>
2019-07-10 14:08:35 -0700

    Finished initialize method

--- handler.rb
+++ handler.rb
@@ -1,12 +1,12 @@
 require 'uploaded_file'
 
 class Resizer
-  def this.image_resizer(event:, context:)
+  def self.image_resizer(event:, context:)
     event = event["Records"].first
     bucket = event["s3"]["bucket"]["name"]
     object = event["s3"]["object"]["key"]
 
-    file = UploadedImage.from_s3(bucket, object)
+    file = from_s3(bucket, object)
     file.resize "50x50"
     file.uploaded_image("resized-images", "resized_" + event["s3"]["object"]["key"])
   end
